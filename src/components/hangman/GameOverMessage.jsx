f2d82a45dca33432ca4c61dbfbecc4bc24b1a1c2
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-08-13 17:31:05 +0300

    game over message

---
+++ src/components/hangman/GameOverMessage.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export default function GameOverMessage({ isWin, onRestart }) {
+  return (
+    <div className="game-over">
+      <p>{isWin ? 'Congratulations, you won!' : 'Sorry, you lost!'}</p>
+      <button onClick={onRestart}>Start New Game</button>
+    </div>
+  );
+}
+
+GameOverMessage.propTypes = {
+  isWin: PropTypes.bool.isRequired, 
+  onRestart: PropTypes.func.isRequired, 
+};

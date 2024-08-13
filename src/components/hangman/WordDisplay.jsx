b3e2576047250dc0150800f997a3405b701580aa
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-08-13 17:11:02 +0300

    WordDisplay changes

--- src/components/hangman/WordDisplay.jsx
+++ src/components/hangman/WordDisplay.jsx
@@ -1,3 +1,25 @@
+import PropTypes from 'prop-types';
+
+export default function WordDisplay({ word, guessedLetters }) {
+  return (
+    <div className="word-display">
+      {word.split('').map((letter, index) => (
+        <span key={index} className="letter">
+          {guessedLetters.includes(letter) ? letter : '_'}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+WordDisplay.propTypes = {
+  word: PropTypes.string.isRequired,
+  guessedLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+
+
+/*
 let vegetables = [
     "agurkas",
     "pomidoras",
@@ -15,3 +37,4 @@ function randomWord() {
 }
 
 export { randomWord }
+*/
